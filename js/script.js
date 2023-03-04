c599091ad0b55f199f14cbe0993c934752beb845
bernardsam-apoh <sb.bernardsam@gmail.com>
2023-03-03 18:36:10 -0800

    Refactored code

--- js/script.js
+++ js/script.js
@@ -1,39 +1,18 @@
 'use strict'
 
-const calc =  (expPercent=50, savPercent=20, invPercent=20, ultPercent=10) => {
-    const amount = 2800
-    const tithe = amount * 0.1
-    
-    const remainingAmount = amount - tithe
-    const expenses = remainingAmount * (expPercent / 100)
-    const savings = remainingAmount * (savPercent / 100)
-    const investments = remainingAmount * (invPercent / 100)
-    const utilities = remainingAmount * (ultPercent / 100)
+const calcExpenses = () => {
+    const { expansesPercent, savingsPercent, investmentPercent, utilitiesPercent } = settings()
 
-    
-
-    const totalPercent = expPercent + savPercent + invPercent + ultPercent
-    console.log(totalPercent+'%')
-
-
-    if (totalPercent === 100) {
-        console.log(tithe)
-        console.log(remainingAmount)
-        console.log(expenses)
-        console.log(savings)
-        console.log(investments)
-        console.log(utilities)
+    if ( expansesPercent || savingsPercent || investmentPercent || utilitiesPercent ) {
+        renderExpeneses(expansesPercent, savingsPercent, investmentPercent, utilitiesPercent)
+        console.log('If block runs')
+    } else {
+        renderExpeneses()
+        console.log('Else block runs')
     }
-
-    if (totalPercent < 100) {
-        console.log('Total percentage less than 100')
-        const extra = 100 - totalPercent
-        console.log(extra)
-    }
-
 }
 
 
-calc(50, 20, 20, 10)
+
 
 
