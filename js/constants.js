51feb9ad1ff629e9046e54985fc907c009a4ef55
bernardsam-apoh <sb.bernardsam@gmail.com>
2023-03-05 02:19:51 -0800

    Created constants file

---
+++ js/constants.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const settingButton = document.querySelector('#setting-button')
+const settingPanel = document.querySelector('#setting-panel')
+const content = document.querySelector('#content')
+
+const amountElement = document.querySelector('#amount-element')
+const titheElement = document.querySelector('#tithe-element')
+const remainingAmountElement = document.querySelector('#remaining-amount-element')
+const expensesElement = document.querySelector('#expenses-element')
+const savingsElement = document.querySelector('#savings-element')
+const investmentsElement = document.querySelector('#investments-element')
+const utilitiesElement = document.querySelector('#utilities-element')
+
+const expansesPercentOption = document.querySelector('#expanses-percent')
+const savingsPercentOption = document.querySelector('#savings-percent')
+const investmentPercentOption = document.querySelector('#investments-percent')
+const utilitiesPercentOption = document.querySelector('#utilities-percent')
